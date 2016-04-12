0288c45bc8b3173f3d53b0c8155093632577cc63
Nan Zhao <nannanwuhui@aliyun.com>
2016-04-12 10:34:24 +0800

    add enum.c

---
+++ enum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    enum E{a,b,c,d};
+    enum E e =100;
+    return 0;
+}
+#if 0
+zn@zn-OptiPlex-9010:~/demo/102015$ gcc enum.c (没有问题)
+zn@zn-OptiPlex-9010:~/demo/102015$ cp enum.c enum.cpp
+zn@zn-OptiPlex-9010:~/demo/102015$ g++ enum.cpp
+enum.cpp: In function ‘int main()’:
+enum.cpp:3:15: error: invalid conversion from ‘int’ to ‘main()::E’ [-fpermissive]
+     enum E e =100;
+               ^
+同样的代码,用c去编译可以通过，但是用c++去编译，c++的编译器就认为类型不一致，
+c++把枚举和整型严格的区分了。
+#endif
