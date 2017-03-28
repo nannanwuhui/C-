42fea47276c48ee5f3edbfe42daa34d8037f7dfe
zn <nan.zhao@bqvision.com>
2017-03-28 11:53:17 +0800

    增加count1.cpp swift::ref_count的使用

---
+++ count1.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <swift/smart_ptr/ref_counted.hpp>
+
+using namespace std;
+
+class MediaSessionImpl : public swift::ref_counted
+{
+public:
+    MediaSessionImpl(){
+        cout << "MediaSessionImpl()" << ":" << get_ref_count() << endl;
+    }
+    ~MediaSessionImpl(){
+        cout << "~MediaSessionImpl()" << ":" << get_ref_count() << endl;
+    }
+};
+
+class MediaSession
+{
+public:
+    MediaSession(int count = 1) : m_pImpl(0),m_count(count){
+        cout << "MediaSession():m_pImpl(0)" << ":" << m_count << endl;
+    }
+    MediaSession(MediaSessionImpl* impl) : m_pImpl(impl){
+        cout << "MediaSession(MediaSessionImpl* impl)!" << ":" << m_pImpl->get_ref_count() << endl;
+    }
+    MediaSession(const MediaSession& other){
+        cout << "MediaSession(const MediaSession& other)" << endl;
+        if(other.m_pImpl)
+            other.m_pImpl->inc_ref();
+        m_pImpl = other.m_pImpl;
+    }
+    ~MediaSession(){
+        cout << "~MediaSession()" << ":" << m_count << ":" << "ref_count:" << m_pImpl->get_ref_count() << endl;
+        if(m_pImpl)
+            m_pImpl->dec_ref();
+    }
+
+    MediaSession& operator= (const MediaSession& other){
+        cout << "operator=" << endl; 
+        if(m_pImpl != other.m_pImpl)   
+        {
+            if(m_pImpl)
+                m_pImpl->dec_ref();
+            if(other.m_pImpl)
+                other.m_pImpl->inc_ref();
+            m_pImpl = other.m_pImpl;
+        }
+        return *this;
+    }
+    void print_ref_count(){
+        cout << "ref_count:" << m_pImpl->get_ref_count() << endl;    
+    }
+private:
+    MediaSessionImpl* m_pImpl;
+    int m_count;
+};
+int main(){
+    MediaSession media(new MediaSessionImpl());
+    //MediaSession media1 = media; 
+    MediaSession media1(media); 
+    media.print_ref_count();
+    media1.print_ref_count();
+    //media = MediaSession();
+    return 0;
+}
+#if 0
+zn@zn-OptiPlex-9010:~/demo/042816$ ls
+a.out  count.cpp  test.cpp
+zn@zn-OptiPlex-9010:~/demo/042816$ g++ count.cpp -I /home/zn/bqvision/clearvision/linux_x86_64/debug/include -lswift-1.0.0-gcc -L/home/zn/bqvision/clearvision/linux_x86_64/debug/lib
+zn@zn-OptiPlex-9010:~/demo/042816$ ./a.out
+./a.out: error while loading shared libraries: libswift-1.0.0-gcc.so: cannot open shared object file: No such file or directory
+zn@zn-OptiPlex-9010:~/demo/042816$ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/zn/bqvision/clearvision/linux_x86_64/debug/lib
+zn@zn-OptiPlex-9010:~/demo/042816$ echo $LD_LIBRARY_PATH
+:/home/zn/bqvision/clearvision/linux_x86_64/debug/lib
+zn@zn-OptiPlex-9010:~/demo/042816$ ./a.out
+MediaSession(MediaSessionImpl* impl)!
+operator=
+~MediaSession()
+~MediaSession()
+zn@zn-OptiPlex-9010:~/demo/042816$
+#endif
